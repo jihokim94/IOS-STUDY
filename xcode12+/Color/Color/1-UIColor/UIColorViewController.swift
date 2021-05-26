50b207ef63f2430695f42e196238e4e8ef97d28a
jihoKim94 <kj941225@gmail.com>
2021-05-27 00:43:53 +0900

    2021-05-27

--- xcode12+/Color/Color/1-UIColor/UIColorViewController.swift
+++ xcode12+/Color/Color/1-UIColor/UIColorViewController.swift
@@ -1,25 +1,4 @@
-//
-//  Mastering iOS
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
+
 
 import UIKit
 
@@ -34,11 +13,36 @@ class UIColorViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     
     @IBAction func sliderChanged(_ sender: UISlider) {
-       
+        let r = CGFloat(redSlider.value)
+        let g = CGFloat(greenSlider.value)
+        let b = CGFloat(blueSlider.value)
+        
+        targetView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var r = CGFloat(0)
+        var g = CGFloat(0)
+        var b = CGFloat(0)
+        var a = CGFloat(0)
+        
+        targetView.backgroundColor?.getRed(&r, green: &g, blue: &b, alpha: &a)
+        redSlider.value = Float(r)
+        blueSlider.value = Float(g)
+        greenSlider.value = Float(b)
+        
+        
+//        targetView.backgroundColor = UIColor.systemBlue
+//
+//        let color = UIColor(red: 29.0 / 255.0, green: 161.0 / 255.0, blue: 242.0 / 255.0 , alpha: 1.0)
+//
+//        UIColor(displayP3Red: 29.0 / 255.0, green: 161.0 / 255.0, blue: 242.0 / 255.0, alpha: 1.0)
+//
+//        UIColor.systemRed.withAlphaComponent(0.5) // 기존색에 알파값만 바꾸고 싶다면
+//
+//        UIColor.clear // 이투명은 백그라운드 컬러를 투명하고 싶을때 주고 씀
         
         
     }
