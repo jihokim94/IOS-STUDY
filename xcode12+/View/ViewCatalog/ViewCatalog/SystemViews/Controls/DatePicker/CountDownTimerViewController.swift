391483a801ccde473a3fb9e37428bfa0c5c1f498
jihoKim94 <kj941225@gmail.com>
2021-05-28 01:52:29 +0900

    2021-05-28

--- xcode12+/View/ViewCatalog/ViewCatalog/SystemViews/Controls/DatePicker/CountDownTimerViewController.swift
+++ xcode12+/View/ViewCatalog/ViewCatalog/SystemViews/Controls/DatePicker/CountDownTimerViewController.swift
@@ -26,18 +26,30 @@ import AudioToolbox
 
 class CountDownTimerViewController: UIViewController {
     
+    var remainTime = 0
     @IBOutlet weak var timeLabel: UILabel!
     
     
     @IBOutlet weak var picker: UIDatePicker!
     
     @IBAction func start(_ sender: Any) {
+        timeLabel.text = "\(Int(picker.countDownDuration))"
         
+        remainTime = Int(picker.countDownDuration)
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            
+            self.remainTime -= 1
+            self.timeLabel.text = "\(self.remainTime)"
+            if self.remainTime == 0 {
+                timer.invalidate() // 타이머 종료 메소드
+                AudioServicesPlaySystemSound(1315)
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        picker.countDownDuration = 60
         
     }
 }
